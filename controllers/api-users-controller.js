91a84232d077fb86258e39536634cb03166772e6
DashkevichAS <antonsnov0604@gmail.com>
2022-12-19 17:16:00 +0300

    hash password & generate token

--- controllers/api-users-controller.js
+++ controllers/api-users-controller.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config.js');
 const handleError = (res, error) => {
   res.status(500).send(error.message);
 };
@@ -15,14 +17,35 @@ const addUser = (req, res) => {
     userRoleString,
   } = req.body;
 
-  console.log('req.body: ', req.body);
+  // User.findOne({ login })
+  //   .then((user) => {
+  //     if (user) {
+  //       return res
+  //         .status(400)
+  //         .json({ message: 'Пользователь с таким Login уже существует' });
+  //     }
+  //   })
+  //   .catch((err) => handleError(res, err));
+
+  // User.findOne({ email })
+  //   .then((user) => {
+  //     if (user) {
+  //       return res
+  //         .status(400)
+  //         .json({ message: 'Пользователь с таким Email уже существует' });
+  //     }
+  //   })
+  //   .catch((err) => handleError(res, err));
+
+  const hashPassword = bcrypt.hashSync(password, 7);
+
   const user = new User({
     birthdayDate,
     email,
     firstName,
     lastName,
     login,
-    password,
+    password: hashPassword,
     userRoleString,
   });
   user
@@ -31,15 +54,32 @@ const addUser = (req, res) => {
     .catch((err) => handleError(res, err));
 };
 
+const generateAccessToken = (id, login) => {
+  const payload = {
+    id,
+    login,
+  };
+  return jwt.sign(payload, secret, { expiresIn: '24' });
+};
+
 const authUser = (req, res) => {
   const { login, password } = req.body;
 
-  User.find({ login, password }, { login, password })
+  User.findOne({ login })
     .then((user) => {
-      if (!user.length) {
-        return res.status(200).send({ error: 'Неверный логин или пороль ' });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: `Пользователь ${login} не найден` });
       }
-      return res.status(200).json(user);
+      const validPassword = bcrypt.compareSync(password, user.password);
+
+      if (!validPassword) {
+        return res.status(400).json({ message: `Введен не верный пароль` });
+      }
+      const token = generateAccessToken(user._id, user.login);
+      console.log('token: ', token);
+      return res.status(200).json({ login: user.login, token });
     })
     .catch((err) => handleError(res, err));
 };
